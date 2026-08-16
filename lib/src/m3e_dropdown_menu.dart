b94e26aac1e9465964cbf518fe07f743b6b289a4
Mudit200408 <muditpurohit2004.pm@gmail.com>
2026-08-16 15:39:36 +0530

    pubspec: migrate to standalone material_ui package for flutter 3.47

--- lib/src/m3e_dropdown_menu.dart
+++ lib/src/m3e_dropdown_menu.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:math' as math;
 
-import 'package:flutter/material.dart';
+import 'package:material_ui/material_ui.dart';
 import 'package:motor/motor.dart';
 
 import 'm3e_dropdown_controller.dart';
